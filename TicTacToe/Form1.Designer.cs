23c79698d756782be0cf3b74f40bdfb97b4d6523
OmkoBass <omkob@hotmail.com>
2019-08-19 19:34:56 +0200

    TicTactoe

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace TicTacToe
+{
+    partial class form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pc0 = new System.Windows.Forms.PictureBox();
+            this.pc1 = new System.Windows.Forms.PictureBox();
+            this.pc2 = new System.Windows.Forms.PictureBox();
+            this.pc5 = new System.Windows.Forms.PictureBox();
+            this.pc4 = new System.Windows.Forms.PictureBox();
+            this.pc3 = new System.Windows.Forms.PictureBox();
+            this.pc8 = new System.Windows.Forms.PictureBox();
+            this.pc7 = new System.Windows.Forms.PictureBox();
+            this.pc6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pc0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pc0
+            // 
+            this.pc0.BackColor = System.Drawing.Color.White;
+            this.pc0.Location = new System.Drawing.Point(12, 12);
+            this.pc0.Name = "pc0";
+            this.pc0.Size = new System.Drawing.Size(150, 150);
+            this.pc0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc0.TabIndex = 0;
+            this.pc0.TabStop = false;
+            this.pc0.Click += new System.EventHandler(this.Pc0_Click);
+            // 
+            // pc1
+            // 
+            this.pc1.BackColor = System.Drawing.Color.White;
+            this.pc1.Location = new System.Drawing.Point(185, 12);
+            this.pc1.Name = "pc1";
+            this.pc1.Size = new System.Drawing.Size(150, 150);
+            this.pc1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc1.TabIndex = 1;
+            this.pc1.TabStop = false;
+            this.pc1.Click += new System.EventHandler(this.Pc1_Click);
+            // 
+            // pc2
+            // 
+            this.pc2.BackColor = System.Drawing.Color.White;
+            this.pc2.Location = new System.Drawing.Point(357, 12);
+            this.pc2.Name = "pc2";
+            this.pc2.Size = new System.Drawing.Size(150, 150);
+            this.pc2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc2.TabIndex = 2;
+            this.pc2.TabStop = false;
+            this.pc2.Click += new System.EventHandler(this.Pc2_Click);
+            // 
+            // pc5
+            // 
+            this.pc5.BackColor = System.Drawing.Color.White;
+            this.pc5.Location = new System.Drawing.Point(357, 183);
+            this.pc5.Name = "pc5";
+            this.pc5.Size = new System.Drawing.Size(150, 150);
+            this.pc5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc5.TabIndex = 5;
+            this.pc5.TabStop = false;
+            this.pc5.Click += new System.EventHandler(this.Pc5_Click);
+            // 
+            // pc4
+            // 
+            this.pc4.BackColor = System.Drawing.Color.White;
+            this.pc4.Location = new System.Drawing.Point(185, 183);
+            this.pc4.Name = "pc4";
+            this.pc4.Size = new System.Drawing.Size(150, 150);
+            this.pc4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc4.TabIndex = 4;
+            this.pc4.TabStop = false;
+            this.pc4.Click += new System.EventHandler(this.Pc4_Click);
+            // 
+            // pc3
+            // 
+            this.pc3.BackColor = System.Drawing.Color.White;
+            this.pc3.Location = new System.Drawing.Point(12, 183);
+            this.pc3.Name = "pc3";
+            this.pc3.Size = new System.Drawing.Size(150, 150);
+            this.pc3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc3.TabIndex = 3;
+            this.pc3.TabStop = false;
+            this.pc3.Click += new System.EventHandler(this.Pc3_Click);
+            // 
+            // pc8
+            // 
+            this.pc8.BackColor = System.Drawing.Color.White;
+            this.pc8.Location = new System.Drawing.Point(357, 359);
+            this.pc8.Name = "pc8";
+            this.pc8.Size = new System.Drawing.Size(150, 150);
+            this.pc8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc8.TabIndex = 8;
+            this.pc8.TabStop = false;
+            this.pc8.Click += new System.EventHandler(this.Pc8_Click);
+            // 
+            // pc7
+            // 
+            this.pc7.BackColor = System.Drawing.Color.White;
+            this.pc7.Location = new System.Drawing.Point(185, 359);
+            this.pc7.Name = "pc7";
+            this.pc7.Size = new System.Drawing.Size(150, 150);
+            this.pc7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc7.TabIndex = 7;
+            this.pc7.TabStop = false;
+            this.pc7.Click += new System.EventHandler(this.Pc7_Click);
+            // 
+            // pc6
+            // 
+            this.pc6.BackColor = System.Drawing.Color.White;
+            this.pc6.Location = new System.Drawing.Point(12, 359);
+            this.pc6.Name = "pc6";
+            this.pc6.Size = new System.Drawing.Size(150, 150);
+            this.pc6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pc6.TabIndex = 6;
+            this.pc6.TabStop = false;
+            this.pc6.Click += new System.EventHandler(this.Pc6_Click);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(524, 521);
+            this.Controls.Add(this.pc8);
+            this.Controls.Add(this.pc7);
+            this.Controls.Add(this.pc6);
+            this.Controls.Add(this.pc5);
+            this.Controls.Add(this.pc4);
+            this.Controls.Add(this.pc3);
+            this.Controls.Add(this.pc2);
+            this.Controls.Add(this.pc1);
+            this.Controls.Add(this.pc0);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(540, 560);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(540, 560);
+            this.Name = "form";
+            this.Text = "X OX";
+            ((System.ComponentModel.ISupportInitialize)(this.pc0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pc6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pc0;
+        private System.Windows.Forms.PictureBox pc1;
+        private System.Windows.Forms.PictureBox pc2;
+        private System.Windows.Forms.PictureBox pc5;
+        private System.Windows.Forms.PictureBox pc4;
+        private System.Windows.Forms.PictureBox pc3;
+        private System.Windows.Forms.PictureBox pc8;
+        private System.Windows.Forms.PictureBox pc7;
+        private System.Windows.Forms.PictureBox pc6;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class form : Form
+    {
+        int potez = 0;
+        bool[] igrano = new bool[9];
+        int[,] xox = new int[3,3];
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private int Win()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if (xox[0, 0] == 1 && xox[0, 1] == 1 && xox[0, 2] == 1) { return 1; }
+                    if (xox[1, 0] == 1 && xox[1, 1] == 1 && xox[1, 2] == 1) { return 1; }
+                    if (xox[2, 0] == 1 && xox[2, 1] == 1 && xox[2, 2] == 1) { return 1; }
+
+                    if (xox[0, 0] == 1 && xox[1, 0] == 1 && xox[2, 0] == 1) { return 1; }
+                    if (xox[0, 1] == 1 && xox[1, 1] == 1 && xox[2, 1] == 1) { return 1; }
+                    if (xox[0, 2] == 1 && xox[1, 2] == 1 && xox[2, 2] == 1) { return 1; }
+
+                    if (xox[0, 0] == 1 && xox[1, 1] == 1 && xox[2, 2] == 1) { return 1; }
+                    if (xox[0, 2] == 1 && xox[1, 1] == 1 && xox[2, 0] == 1) { return 1; }
+
+                    if (xox[0, 0] == 2 && xox[0, 1] == 2 && xox[0, 2] == 2) { return 0; }
+                    if (xox[1, 0] == 2 && xox[1, 1] == 2 && xox[1, 2] == 2) { return 0; }
+                    if (xox[2, 0] == 2 && xox[2, 1] == 2 && xox[2, 2] == 2) { return 0; }
+
+                    if (xox[0, 0] == 2 && xox[1, 0] == 2 && xox[2, 0] == 2) { return 0; }
+                    if (xox[0, 1] == 2 && xox[1, 1] == 2 && xox[2, 1] == 2) { return 0; }
+                    if (xox[0, 2] == 2 && xox[1, 2] == 2 && xox[2, 2] == 2) { return 0; }
+
+                    if (xox[0, 0] == 2 && xox[1, 1] == 2 && xox[2, 2] == 2) { return 0; }
+                    if (xox[0, 2] == 2 && xox[1, 1] == 2 && xox[2, 0] == 2) { return 0; }
+                }
+            }
+            return 2;
+        }
+
+        private void Pc0_Click(object sender, EventArgs e)
+        {
+            if(potez % 2 == 0 && igrano[0] == false)
+            {
+                pc0.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[0] = true;
+                xox[0, 0] = 1;
+            }
+            else if(potez % 2 == 1 && igrano[0] == false)
+            {
+                pc0.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[0] = true;
+                xox[0, 0] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if(Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc1_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[1] == false)
+            {
+                pc1.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[1] = true;
+                xox[0, 1] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[1] == false)
+            {
+                pc1.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[1] = true;
+                xox[0, 1] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc2_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[2] == false)
+            {
+                pc2.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[2] = true;
+                xox[0, 2] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[2] == false)
+            {
+                pc2.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[2] = true;
+                xox[0, 2] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc3_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[3] == false)
+            {
+                pc3.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[3] = true;
+                xox[1, 0] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[3] == false)
+            {
+                pc3.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[3] = true;
+                xox[1, 0] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc4_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[4] == false)
+            {
+                pc4.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[4] = true;
+                xox[1, 1] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[4] == false)
+            {
+                pc4.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[4] = true;
+                xox[1, 1] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc5_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[5] == false)
+            {
+                pc5.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[5] = true;
+                xox[1, 2] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[5] == false)
+            {
+                pc5.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[5] = true;
+                xox[1, 2] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc6_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[6] == false)
+            {
+                pc6.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[6] = true;
+                xox[2, 0] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[6] == false)
+            {
+                pc6.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[6] = true;
+                xox[2, 0] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc7_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[7] == false)
+            {
+                pc7.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[7] = true;
+                xox[2, 1] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[7] == false)
+            {
+                pc7.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[7] = true;
+                xox[2, 1] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+
+        private void Pc8_Click(object sender, EventArgs e)
+        {
+            if (potez % 2 == 0 && igrano[8] == false)
+            {
+                pc8.Image = Image.FromFile("multiply_64px.png");
+                potez++; igrano[8] = true;
+                xox[2, 2] = 1;
+            }
+            else if (potez % 2 == 1 && igrano[8] == false)
+            {
+                pc8.Image = Image.FromFile("circle_64px.png");
+                potez++; igrano[8] = true;
+                xox[2, 2] = 2;
+            }
+            if (Win() == 1) { MessageBox.Show("X je pobedio!"); }
+            else if (Win() == 0) { MessageBox.Show("OX je pobedio!"); }
+        }
+    }
+}
